d736b3c1cb65653d4a0dbc5725a45f494528485d
Yyunozor <yyunozor@icloud.com>
2024-11-05 21:46:49 +0100

    works

--- src/bonus/x_bonus.c
+++ src/bonus/x_bonus.c
@@ -5,33 +5,36 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: anpayot <anpayot@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/05 16:40:44 by anpayot           #+#    #+#             */
-/*   Updated: 2024/11/05 16:41:55 by anpayot          ###   ########.fr       */
+/*   Created: 2024/11/05 21:44:25 by anpayot           #+#    #+#             */
+/*   Updated: 2024/11/05 21:44:51 by anpayot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	x_prefix(t_printf *p, const char *prefix)
+void x_prefix(t_printf *p, const char *prefix)
 {
 	p->len += write(1, prefix, ft_strlen(prefix));
 }
 
-void	x_number(t_printf *p, const char *str)
+
+void x_number(t_printf *p, const char *str)
 {
 	p->len += write(1, str, ft_strlen(str));
 }
 
-void	x_padding(t_printf *p, int len, char pad_char)
+
+void x_padding(t_printf *p, int len, char pad_char)
 {
-	while (len < p->width)
+	int padding_len = p->width - len;  // Calculate the exact padding required
+	while (padding_len > 0)
 	{
 		p->len += write(1, &pad_char, 1);
-		len++;
+		padding_len--;
 	}
 }
 
-void	x_precision(t_printf *p, int num_len)
+void x_precision(t_printf *p, int num_len)
 {
 	while (num_len < p->precision)
 	{
--- src/conversions/x_int.c
+++ src/conversions/x_int.c
@@ -5,36 +5,44 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: anpayot <anpayot@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/05 17:19:33 by anpayot           #+#    #+#             */
-/*   Updated: 2024/11/05 20:51:21 by anpayot          ###   ########.fr       */
+/*   Created: 2024/11/05 21:45:03 by anpayot           #+#    #+#             */
+/*   Updated: 2024/11/05 21:46:34 by anpayot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void x_int(t_printf *p)
+void	x_int(t_printf *p)
 {
-	int num;
-	char *str;
-	char *prefix = "";
-	int len;
-	int num_len;
+	long long	num;
+	char		*str;
+	const char	*prefix = "";
+	int 		len; 
+	int			num_len;
 
 	num = va_arg(p->ap, int);
+
+	// Determine the prefix based on sign and flags
 	if (num < 0)
 	{
 		prefix = "-";
 		num = -num;
 	}
-	else if (num >= 0 && p->flags.plus)
+	else if (p->flags.plus)
 		prefix = "+";
-	else if (num >= 0 && p->flags.space)
+	else if (p->flags.space)
 		prefix = " ";
-	str = ft_itoa_base(num, "0123456789");
+
+	// Convert number to string
+	str = ft_ulltoa_base(num, "0123456789");
 	num_len = ft_strlen(str);
 	len = num_len + ft_strlen(prefix);
-	if (p->precision >= 0 && p->precision > num_len)
+
+	// Adjust total length for precision if it's greater than num_len
+	if (p->precision > num_len)
 		len = p->precision + ft_strlen(prefix);
+
+	// Left alignment
 	if (p->flags.minus)
 	{
 		x_prefix(p, prefix);
@@ -46,16 +54,19 @@ void x_int(t_printf *p)
 	{
 		if (p->flags.zero && p->precision < 0)
 		{
+			// Zero-padding without precision
 			x_prefix(p, prefix);
 			x_padding(p, len, '0');
 		}
 		else
 		{
+			// Space-padding with precision or without zero flag
 			x_padding(p, len, ' ');
 			x_prefix(p, prefix);
 		}
 		x_precision(p, num_len);
 		x_number(p, str);
 	}
+
 	free(str);
 }
